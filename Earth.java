9a1415fe85ec1497586c229f056017e332751320
renukakolli <renukakolli33@gmail.com>
2021-07-29 15:48:39 +0530

    program

---
+++ Earth.java
@@ -0,0 +1,16 @@
+package earth;
+
+import java.util.Scanner;
+
+public class Earth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner sc=new Scanner(System.in);
+		double value=5.972*Math.pow(10, 24);
+		
+		System.out.println(value);
+
+	}
+
+}
